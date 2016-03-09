27fbdbb052c068a144fda8e160f5378e2ec3d5a3
Rob Fletcher <rfletcher@netflix.com>
2016-03-09 00:02:09 -0800

    Kotlin programmers do it with method refs

--- src/main/kotlin/day5/day5.kt
+++ src/main/kotlin/day5/day5.kt
@@ -4,22 +4,22 @@ val input = ClassLoader.getSystemResource("day5.txt").readText()
 
 fun main(vararg args: String) {
   val words = input.lines()
-  println("${words.count { it.nice1() }} of ${words.size} words are nice (original rules)")
-  println("${words.count { it.nice2() }} of ${words.size} words are nice (revised rules)")
+  println("${words.count(::nice1)} of ${words.size} words are nice (original rules)")
+  println("${words.count(::nice2)} of ${words.size} words are nice (revised rules)")
 }
 
 val vowels = "aeiou".toCharArray()
-val doubleLetters = Regex("([a-z])\\1")
-val bannedPhrases = Regex("(ab)|(cd)|(pq)|(xy)")
+val doubles = Regex("([a-z])\\1")
+val banned = Regex("(ab)|(cd)|(pq)|(xy)")
 
-fun String.nice1() =
-  count { it in vowels } > 2
-    && doubleLetters.containsMatchIn(this)
-    && !bannedPhrases.containsMatchIn(this)
+fun nice1(s: String) =
+  s.count { it in vowels } > 2
+    && doubles.containsMatchIn(s)
+    && !banned.containsMatchIn(s)
 
-val twoPairs = Regex("([a-z][a-z]).*\\1")
-val palindrome = Regex("([a-z]).\\1")
+val twoPair = Regex("([a-z][a-z])[a-z]*\\1")
+val sandwich = Regex("([a-z])[a-z]\\1")
 
-fun String.nice2() =
-  twoPairs.containsMatchIn(this)
-    && palindrome.containsMatchIn(this)
+fun nice2(s: String) =
+  twoPair.containsMatchIn(s)
+    && sandwich.containsMatchIn(s)
